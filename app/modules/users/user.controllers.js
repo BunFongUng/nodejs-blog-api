cfc7d44e35bcc0850d6acb8447bd8166a8f6f7fb
Lester <lester.ung@pathmazing.com>
2017-08-23 15:59:53 +0700

    using async and await

--- app/modules/users/user.controllers.js
+++ app/modules/users/user.controllers.js
@@ -1,15 +1,17 @@
 import User from './user.model';
 import _ from 'lodash';
 
-export function signUp(req, res) {
-	let body = _.pick(req.body, ['firstName', 'lastName', 'userName', 'email', 'password']);
-	let user = new User(body);
+export async function signUp(req, res, next) {
+	try {
+		let body = _.pick(req.body, ['firstName', 'lastName', 'userName', 'email', 'password']);
+		let user = new User(body);
 
-	user.save().then(() => {
-		return user.generateAuthToken();
-	}).then(token => {
+		await user.save();
+		let token = await user.generateAuthToken();
+
 		res.header('x-auth', token).send(user);
-	}).catch(err => {
-		res.status(400).send(err);
-	});
+	} catch (e) {
+		res.status(400).send(e);
+		next(e)
+	}
 }
